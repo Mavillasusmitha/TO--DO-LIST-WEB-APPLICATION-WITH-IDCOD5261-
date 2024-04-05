70eca9566057dea43480c8c86a0defbdf6171621
Mavillasusmitha <165757262+Mavillasusmitha@users.noreply.github.com>
2024-04-05 12:31:06 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+<link rel="stylesheet" href="si.css">
+<div id="myDIV" class="header">
+    <h2>My To Do List</h2>
+    <input type="text" id="myInput" placeholder="Add tasks">
+    <span onclick="newElement()" class="addBtn">Add</span>
+  </div>
+  
+  <ul id="myUL">
+    <li>Positions</li>
+    <li class="checked">Non IT</li>
+    <li>IT</li>
+    <li class="checked">Hardware</li>
+    <li>Software</li>
+    <li>office</li>
+  </ul>
+  <script src="script.js"></script>
